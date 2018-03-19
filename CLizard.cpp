65b8684992b03a8fcc9f743af2f6ee86220f36bc
DariaFil <36414448+DariaFil@users.noreply.github.com>
2018-03-19 08:51:53 +0300

    Fixed codestyle

--- CLizard.cpp
+++ CLizard.cpp
@@ -1,6 +1,8 @@
 #include "CLizard.h"
 
-void CLizardInfantryman::info() const {	cout << "Lizard Infantryman" << endl; }
+void CLizardInfantryman::info() const {
+	cout << "Lizard Infantryman" << endl;
+}
 void CLizardInfantryman::race_protection_bonus(const string& field_type) {
 	if (field_type == "Water" && !InField) {
 		PROTECTION += 10;
@@ -11,9 +13,13 @@ void CLizardInfantryman::race_protection_bonus(const string& field_type) {
 		InField = false;
 	}
 }
-string CLizardInfantryman::return_name() const { return "LI"; }
+string CLizardInfantryman::return_name() const {
+	return "LI";
+}
 
-void CLizardArcher::info() const { cout << "Lizard Archer" << endl; }
+void CLizardArcher::info() const {
+	cout << "Lizard Archer" << endl;
+}
 void CLizardArcher::race_protection_bonus(const string& field_type) {
 	if (field_type == "Water" && !InField) {
 		PROTECTION += 10;
@@ -24,9 +30,13 @@ void CLizardArcher::race_protection_bonus(const string& field_type) {
 		InField = false;
 	}
 }
-string CLizardArcher::return_name() const { return "LA"; }
+string CLizardArcher::return_name() const {
+	return "LA";
+}
 
-void CLizardHorseman::info() const { cout << "Lizard Horseman" << endl; }
+void CLizardHorseman::info() const {
+	cout << "Lizard Horseman" << endl;
+}
 void CLizardHorseman::race_protection_bonus(const string& field_type) {
 	if (field_type == "Water" && !InField) {
 		PROTECTION += 10;
@@ -37,9 +47,13 @@ void CLizardHorseman::race_protection_bonus(const string& field_type) {
 		InField = false;
 	}
 }
-string CLizardHorseman::return_name() const { return "LH"; }
+string CLizardHorseman::return_name() const {
+	return "LH";
+}
 
-void CLizardBerserk::info() const { cout << "Lizard Berserk" << endl; }
+void CLizardBerserk::info() const {
+	cout << "Lizard Berserk" << endl;
+}
 void CLizardBerserk::race_protection_bonus(const string& field_type) {
 	if (field_type == "Water" && !InField) {
 		PROTECTION += 10;
@@ -50,4 +64,6 @@ void CLizardBerserk::race_protection_bonus(const string& field_type) {
 		InField = false;
 	}
 }
-string CLizardBerserk::return_name() const { return "LB"; }
+string CLizardBerserk::return_name() const {
+	return "LB";
+}
