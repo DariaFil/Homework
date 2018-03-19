03edfbd3ddf583ff6cad28b2a853068c3cd41004
DariaFil <36414448+DariaFil@users.noreply.github.com>
2018-03-19 08:39:18 +0300

    Fixed codestyle

--- Mygame.cpp
+++ Mygame.cpp
@@ -1,14 +1,12 @@
 #include "CGame.h"
 
-bool isParam(string line)
-{
+bool isParam(string line) {
 	char* p;
 	strtol(line.c_str(), &p, 10);
 	return *p == 0;
 }
 
-bool right_cin(string data, int min_param, int max_param)
-{
+bool right_cin(string data, int min_param, int max_param) {
 	if (data.size() < 9 && isParam(data) && stoi(data) >= min_param && stoi(data) <= max_param)
 		return true;
 	else 
