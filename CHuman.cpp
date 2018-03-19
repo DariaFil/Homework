5bce1fed19678eaaf142c762a3f450070518d7da
DariaFil <36414448+DariaFil@users.noreply.github.com>
2018-03-19 08:49:45 +0300

    Fixed codestyle

--- CHuman.cpp
+++ CHuman.cpp
@@ -1,6 +1,8 @@
 #include "CHuman.h"
 
-void CHumanInfantryman::info() const { cout << "Human Infantryman" << endl; }
+void CHumanInfantryman::info() const {
+	cout << "Human Infantryman" << endl;
+}
 void CHumanInfantryman::race_protection_bonus(const string& field_type) {
 	if (field_type == "Forest" && !InField) {
 		PROTECTION += 10;
@@ -11,9 +13,13 @@ void CHumanInfantryman::race_protection_bonus(const string& field_type) {
 		InField = false;
 	}
 }
-string CHumanInfantryman::return_name() const { return "HI"; }
+string CHumanInfantryman::return_name() const {
+	return "HI";
+}
 
-void CHumanArcher::info() const { cout << "Human Archer" << endl; }
+void CHumanArcher::info() const {
+	cout << "Human Archer" << endl;
+}
 void CHumanArcher::race_protection_bonus(const string& field_type) {
 	if (field_type == "Forest" && !InField) {
 		PROTECTION += 10;
@@ -24,9 +30,13 @@ void CHumanArcher::race_protection_bonus(const string& field_type) {
 		InField = false;
 	}
 }
-string CHumanArcher::return_name() const { return "HA"; }
+string CHumanArcher::return_name() const {
+	return "HA";
+}
 
-void CHumanHorseman::info() const {	cout << "Human Horseman" << endl; }
+void CHumanHorseman::info() const {
+	cout << "Human Horseman" << endl;
+}
 void CHumanHorseman::race_protection_bonus(const string& field_type) {
 	if (field_type == "Forest" && !InField) {
 		PROTECTION += 10;
@@ -37,9 +47,13 @@ void CHumanHorseman::race_protection_bonus(const string& field_type) {
 		InField = false;
 	}
 }
-string CHumanHorseman::return_name() const { return "HH"; }
+string CHumanHorseman::return_name() const {
+	return "HH";
+}
 
-void CHumanBerserk::info() const { cout << "Human Berserk" << endl; }
+void CHumanBerserk::info() const {
+	cout << "Human Berserk" << endl;
+}
 void CHumanBerserk::race_protection_bonus(const string& field_type) {
 	if (field_type == "Forest" && !InField) {
 		PROTECTION += 10;
@@ -50,4 +64,6 @@ void CHumanBerserk::race_protection_bonus(const string& field_type) {
 		InField = false;
 	}
 }
-string CHumanBerserk::return_name() const { return "HB"; }
+string CHumanBerserk::return_name() const {
+	return "HB";
+}
